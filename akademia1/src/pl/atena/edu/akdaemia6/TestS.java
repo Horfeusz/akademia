b6019b0d9387e93dc16d074f712ffdafc13bfe0f
Horfeusz <horfeusz@gmail.com>
2017-10-21 20:50:14 +0200

    Odczyt dokumentu

---
+++ akademia1/src/pl/atena/edu/akdaemia6/TestS.java
@@ -0,0 +1,11 @@
+package pl.atena.edu.akdaemia6;
+
+public class TestS {
+
+	public static void main(String[] args) {
+		String test = "kropka.";
+		System.out.println(test.replace(".", ""));
+
+	}
+
+}
